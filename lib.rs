37c46ae714b141143c209035832ec54bbdebc1d2
susu0929 <1521018128@qq.com>
2022-10-06 00:14:41 +0800

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -4,71 +4,91 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod erc20 {
+	use ink_env::AccountId;
+	use ink_storage::Mapping;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct Erc20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    struct ERC20 {
+		total_supply: Balance,
+		balance: Mapping<AccountId, Balance>,
+		approval: Mapping<(AccountId, AccountId), Balance>
+	}
+
+	#[ink(event)]
+	struct Transfer{
+		#[ink(topic)]
+		from: AccountId,
+		to:AccountId,
+		value:Balance;
+	}
+
+	#[ink(event)]
+	struct Approval {
+		owner: AccountId,
+		spender: AccountId,
+		value: Balance
+	}
+
+	impl ERC20 {
+		#[ink(connstructor)]
+		pub fn new(total_supply: Balance) -> Self {
+		    let mut balances Mapping<AccountId,7>= Mapping::default();
+
+
+
+
+		    let sender = Self::env().sender();
+		    balance,insert(key &sender, value total_supply);
+
+
+		    Self::env().cmit_event(
+		     Transfer {
+		          from: AccountId::default(),
+				  to:sender,
+				 value:total_supply
+		     }
+		};
+
+		    Self{
+		    total_supply,
+		    balances,
+		    approval: Default::default(),
+		    }
+		)
+
+		#[ink(message)]
+		pub fn total_supply(&self) -> Balance {
+			self.total_supply
+		}
+
+		#[ink(message)]
+		pub fn balance_of(&self, who:AccountId) -> Balance {
+		    self.balance.get( key &who).unmrap_or_default()
+	    }
+
+		// todo:define pub enum Error()
+		#[ink(message)]
+		pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<(),String> {
+			let from = self.env().sender();
+			let from_balance = self.balance_of( from );
+			if from_balance < value{
+				return Err("Insufficient Balance".to_camed());
+			}
+
+
+			self.balance.insert( key: &from, value &(from_balance = value));
+
+		}
+
+           self.emv().cmit_event(
+           Transfer{
+           from:sender,
+           to,
+           value
+        }
+    };
+
+    Ok(())
     }
-
-    impl Erc20 {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let erc20 = Erc20::default();
-            assert_eq!(erc20.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut erc20 = Erc20::new(false);
-            assert_eq!(erc20.get(), false);
-            erc20.flip();
-            assert_eq!(erc20.get(), true);
-        }
-    }
-}
+		
+  
